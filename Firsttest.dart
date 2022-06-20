0948526d91c5a5eed2992d76f06335a5f1dd480b
Diresh Ramburan <78588945+Shallcross123@users.noreply.github.com>
2022-06-20 14:17:50 +0200

    Add files via upload

---
+++ Firsttest.dart
@@ -0,0 +1,11 @@
+void main() {
+
+  String Name ='John';
+  String App = 'App1';
+  String City = 'Durban';
+  
+ print("My Name is $Name ");
+ print("My Favorite App is $App ");
+ print("I live in  $City ");
+  
+} 
